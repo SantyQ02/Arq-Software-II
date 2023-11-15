c1a8a010519e42246b3f9ba660c2369693ca4d82
Giovanni Borgogno <giovaborgogno@gmail.com>
2023-11-15 19:17:13 -0300

    implemented local cache as a client

--- search/main.go
+++ search/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"mvc-go/app"
 	"mvc-go/solr"
+	cc "mvc-go/cache"
 	"mvc-go/utils/queue"
 )
 
 func main() {
 	solr.StartSolr()
+	cc.StartLocalCache()
 	go queue.Consumer()
 	app.StartRoute()
 }
