3c948160b3cd0333939ac80bb6724ad543eb9b3d
Giovanni Borgogno <giovaborgogno@gmail.com>
2023-11-16 19:40:30 -0300

    frontend containers loaders

--- frontend_containers/src/components/admin/ContainersList.js
+++ frontend_containers/src/components/admin/ContainersList.js
@@ -1,31 +1,85 @@
-import { useEffect } from "react"
+import { Oval } from 'react-loader-spinner'
 import { TrashIcon, RefreshIcon, StopIcon, PlayIcon } from '@heroicons/react/solid'
 import Image from "next/image"
 
 import { startContainer, restartContainer, deleteContainer, stopContainer } from "@/lib/api/containers"
+import { useState } from 'react'
+
+const ContainerRow = ({ container }) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const actions = {
+    start: async (id) => await startContainer(id),
+    delete: async (id) => await deleteContainer(id),
+    stop: async (id) => await stopContainer(id),
+    restart: async (id) => await restartContainer(id),
+
+  }
+  const handleContainer = async (action, id) => {
+    setIsLoading(true)
+    await actions[action](id)
+    setIsLoading(false)
+  }
+
+  return (
+    <li key={container.container_id} className="grid grid-cols-12 gap-x-6 py-2 text-sm">
+      <div className="col-span-3 flex gap-x-4 items-center">
+        <Image
+          src={'/images/docker_container_icon.png'}
+          width={35}
+          height={35}
+        />
+        <div className="min-w-0 flex-auto">
+          <p className="text-sm font-semibold leading-6 text-gray-900">{container.name.toLowerCase()} {container.memory_usage == '0B' ? "(Paused)" : ""}</p>
+          <div className="flex">
+
+            <p className="mt-1 truncate text-xs leading-5 text-gray-500">{container.container_id}</p>
+            {/* <p className="ml-1 mt-1 truncate text-xs leading-5 text-gray-500"> ~ {container.role.toUpperCase()}</p> */}
+          </div>
+        </div>
+      </div>
+      <div className=" text-end">
+        {container.cpu}
+      </div>
+      <div className="col-span-2 text-end">
+        {container.memory_usage} / {container.memory_limit}
+      </div>
+      <div className="col-span-2 text-end">
+        {container.memory}
+      </div>
+      <div className="col-span-2 text-end">
+        {container.net_i} / {container.net_o}
+      </div>
+
+      <div className="col-span-2 flex justify-end gap-3">
+        {
+          !isLoading ?
+            <>
+              <PlayIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleContainer("start", container.container_id)} />
+              <StopIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleContainer("stop", container.container_id)} />
+              <RefreshIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleContainer("restart", container.container_id)} />
+              <TrashIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleContainer("delete", container.container_id)} />
+            </>
+            :
+            <Oval
+              type="Oval"
+              color="#000"
+              width={20}
+              height={20}
+            />
+        }
+
+      </div>
+    </li>
+  )
+}
 
 
 export default function ContainersList({ containers, refresh, setRefresh }) {
 
-  const handleStartContainer = async (id) => {
-    await startContainer(id)
-    setRefresh(!refresh)
-  }
 
-  const handleDeleteContainer = async (id) => {
-    await deleteContainer(id)
-    setRefresh(!refresh)
-  }
 
-  const handleReStartContainer = async (id) => {
-    await restartContainer(id)
-    setRefresh(!refresh)
-  }
 
-  const handleStopContainer = async (id) => {
-    await stopContainer(id)
-    setRefresh(!refresh)
-  }
 
 
   return (
@@ -61,64 +115,9 @@ export default function ContainersList({ containers, refresh, setRefresh }) {
       </div>
       <ul role="list" className="divide-y divide-gray-100 pt-3">
         {containers != null && containers.map((container) => (
-          <li key={container.container_id} className="grid grid-cols-12 gap-x-6 py-2 text-sm">
-            <div className="col-span-3 flex gap-x-4 items-center">
-              <Image
-                src={'/images/docker_container_icon.png'}
-                width={35}
-                height={35}
-              />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{container.name.toLowerCase()} {container.memory_usage == '0B' ? "(Paused)" : ""}</p>
-                <div className="flex">
-
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{container.container_id}</p>
-                  {/* <p className="ml-1 mt-1 truncate text-xs leading-5 text-gray-500"> ~ {container.role.toUpperCase()}</p> */}
-                </div>
-              </div>
-            </div>
-            <div className=" text-end">
-              {container.cpu}
-            </div>
-            <div className="col-span-2 text-end">
-              {container.memory_usage} / {container.memory_limit}
-            </div>
-            <div className="col-span-2 text-end">
-              {container.memory}
-            </div>
-            <div className="col-span-2 text-end">
-              {container.net_i} / {container.net_o}
-            </div>
-
-            <div className="col-span-2 flex justify-end gap-3">
-              <PlayIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleStartContainer(container.container_id)}/>
-              <StopIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true"onClick={() => handleStopContainer(container.container_id)} />
-              <RefreshIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleReStartContainer(container.container_id)}/>
-              <TrashIcon className="flex-shrink-0 h-5 w-5 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleDeleteContainer(container.container_id)}/>
-            </div>
-            {/* <div className=" sm:flex sm:flex-col sm:items-center ">
-              <p className="text-sm leading-6 text-gray-900">Active</p>
-
-
-              {container.active ?
-
-                <label class="relative inline-flex items-center cursor-pointer"
-                  onClick={e => handleCancelUser(e, user)}>
-                  <input type="checkbox" checked={true} class="sr-only peer" />
-                  <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
-                  <span class=" text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-                </label>
-                :
-                <label class="relative inline-flex items-center cursor-pointer"
-                  onClick={e => handleRegisterUser(e, user)}>
-                  <input type="checkbox" checked={false} class="sr-only peer" />
-                  <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
-                  <span class=" text-sm font-medium text-gray-900 dark:text-gray-300"></span>
-                </label>
-              }
-            </div> */}
-          </li>
-        ))}
+          <ContainerRow container={container} />
+        ))
+        }
 
       </ul>
     </div>
--- frontend_containers/src/pages/index.js
+++ frontend_containers/src/pages/index.js
@@ -1,8 +1,7 @@
-import ContainersList from "@/components/admin/ContainersList"
 import DashboardAdmin from "@/layouts/DashboardAdmin"
-import { getContainersStats, createContainer } from "@/lib/api/containers"
+import { getContainersStats } from "@/lib/api/containers"
 import { useEffect, useState } from "react"
-import { PlusIcon, MinusIcon } from '@heroicons/react/solid'
+import ServiceRow from "@/components/admin/ServiceRow"
 
 const filtered_containers = [
   {
@@ -35,45 +34,7 @@ const filtered_containers = [
 
 const Dashboard = () => {
   const [containers, setContainers] = useState(null);
-  const [openSearch, setOpenSearch] = useState(false);
-  const [openBusiness, setOpenBusiness] = useState(false);
-  const [openOthers, setOpenOthers] = useState(false);
-  const [openHotels, setOpenHotels] = useState(false);
-  const [openFrontend, setOpenFrontend] = useState(false);
   const [refresh, setRefresh] = useState(false)
-
-  const dic = {
-    frontend: {
-      setOpen: () => setOpenFrontend(!openFrontend),
-      isOpen: () => openFrontend
-    },
-    search: {
-      setOpen: () => setOpenSearch(!openSearch),
-      isOpen: () => openSearch
-    },
-    business: {
-      setOpen: () => setOpenBusiness(!openBusiness),
-      isOpen: () => openBusiness
-    },
-    hotels: {
-      setOpen: () => setOpenHotels(!openHotels),
-      isOpen: () => openHotels
-    },
-    others: {
-      setOpen: () => setOpenOthers(!openOthers),
-      isOpen: () => openOthers
-    },
-  }
-
-  const handleOpen = (service) => {
-    dic[service]["setOpen"]()
-    console.log(dic[service]["isOpen"]())
-  }
-
-  const handleNewInstance = async (service) => {
-    await createContainer(service)
-    setRefresh(!refresh)
-  }
 
   useEffect(() => {
 
@@ -118,23 +79,8 @@ const Dashboard = () => {
       {/* Replace with your content */}
       <div className="divide-y-2 divide-dashed divide-gray-200">
         {containers && filtered_containers.map(item =>
-          <div className="pt-5 divide-y-2 divide-dashed">
-            <div className="mb-5 font-lg font-bold flex justify-between items-center">
-              <div className="flex-1">{item.title}</div>
-              <div className={`flex-1 cursor-pointer font-normal hover:text-blue-800 ${item.service == 'others' && 'hidden'}`} onClick={() => handleNewInstance(item.service)}>Create new instance</div>
-              {
-                dic[item.service]["isOpen"]() ?
-                  <MinusIcon className="flex-shrink-0 h-7 w-7 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleOpen(item.service)} />
-                  :
-                  <PlusIcon className="flex-shrink-0 h-7 w-7 text-blue-400 cursor-pointer" aria-hidden="true" onClick={() => handleOpen(item.service)} />
-
-              }
-            </div>
-            <div className={`${dic[item.service]["isOpen"]() ? '' : 'hidden'}`}>
-
-              <ContainersList containers={item.containers(containers)} refresh={refresh} setRefresh={setRefresh} />
-            </div>
-          </div>)}
+          <ServiceRow item={item} refresh={refresh} setRefresh={setRefresh} containers={containers} />
+          )}
       </div>
       {/* /End replace */}
 
