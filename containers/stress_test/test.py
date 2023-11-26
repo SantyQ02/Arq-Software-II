6aeee24f1c394680ad0c9d87436c4794d4b186a1
Giovanni Borgogno <giovaborgogno@gmail.com>
2023-11-26 00:40:40 -0300

    stress test for frontend

--- containers/stress_test/test.py
+++ containers/stress_test/test.py
@@ -4,7 +4,7 @@ from time import sleep
 print('Working...')
 while(True):
     try:
-        requests.get('http://localhost:83/api/search?city=9&check_in_date=2023-11-21&check_out_date=2023-11-28')
+        requests.get('http://localhost:81')
     except:
         sleep(2)
     
